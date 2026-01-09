87b1b0a1f3a071535c9668e457093831a806ebf4
rickbeeloo <r.beeloo@outlook.com>
2026-01-09 13:50:10 +0100

    try `rayon`

--- helicase/src/paraseq_reader.rs
+++ helicase/src/paraseq_reader.rs
@@ -1,9 +1,12 @@
-use paraseq::ProcessError;
-
 use crate::{
     Config, FastxParser,
     input::{FromSlice, InputData, MmapInput},
 };
+use paraseq::ProcessError;
+use rayon::ThreadPoolBuilder;
+use rayon::iter::IntoParallelIterator;
+
+use rayon::prelude::*;
 
 pub struct ParallelHelicaseReader<'a, const CONFIG: Config> {
     mmap: MmapInput<'a>,
@@ -37,24 +40,26 @@ fn find_fasta_boundary(data: &[u8], mut pos: usize) -> usize {
 fn find_fastq_boundary(data: &[u8], mut pos: usize) -> usize {
     while let Some(offset) = memchr::memchr(b'\n', &data[pos..]) {
         let abs_pos = pos + offset;
-        // \n must be followed by '@'
-        if abs_pos + 1 >= data.len() || data[abs_pos + 1] != b'@' {
-            pos = abs_pos + 1;
+        let next_pos = abs_pos + 1;
+
+        // If we are at the end, this was quality line (continue)
+        if next_pos >= data.len() || data[next_pos] != b'@' {
+            pos = next_pos;
             continue;
         }
-        // Check next line after '@'
-        let remaining = &data[abs_pos..];
-        // Search for the newline after the '@' (skipping \n and @)
-        if let Some(newline_offset) = memchr::memchr(b'\n', &remaining[2..]) {
-            let next_line_start = newline_offset + 3; // +2 offset + 1 for the \n itself
-            // Continue search if we have a next line that starts with '@'
-            // i.e. we have quality scores instead of valid record
+
+        // If we have a '@' at the next line, this was quality line (continue)
+        let remaining = &data[next_pos..];
+        if let Some(newline_offset) = memchr::memchr(b'\n', &remaining[1..]) {
+            let next_line_start = newline_offset + 2;
             if next_line_start < remaining.len() && remaining[next_line_start] == b'@' {
-                pos = abs_pos + 1;
+                pos = next_pos;
                 continue;
             }
         }
-        return abs_pos + 1;
+
+        // We made it here, so valid fastq returns positions
+        return next_pos;
     }
     data.len()
 }
@@ -66,44 +71,54 @@ impl<'b, const CONFIG: Config> paraseq::parallel::ParallelReader
 
     fn process_parallel<T>(self, processor: &mut T, num_threads: usize) -> paraseq::Result<()>
     where
-        T: for<'a> paraseq::prelude::ParallelProcessor<Self::Rf<'a>>,
+        T: for<'a> paraseq::prelude::ParallelProcessor<Self::Rf<'a>> + Clone + Send,
     {
         let data = self.mmap.data();
         assert!(data[0] == b'>' || data[0] == b'@');
         let is_fastq = data[0] == b'@';
 
-        std::thread::scope(|scope| {
-            let chunk_size = data.len().div_ceil(num_threads);
+        let chunk_size = data.len().div_ceil(num_threads);
 
-            let mut splits = (0..=num_threads)
-                .map(|i| (i * chunk_size).min(data.len()))
-                .collect::<Vec<usize>>();
+        let mut splits = (0..=num_threads)
+            .map(|i| (i * chunk_size).min(data.len()))
+            .collect::<Vec<usize>>();
 
-            let len = splits.len();
-            for split in &mut splits[1..len - 1] {
-                *split = if is_fastq {
-                    find_fastq_boundary(data, *split)
-                } else {
-                    find_fasta_boundary(data, *split)
-                };
-            }
+        let len = splits.len();
+        for split in &mut splits[1..len - 1] {
+            *split = if is_fastq {
+                find_fastq_boundary(data, *split)
+            } else {
+                find_fasta_boundary(data, *split)
+            };
+        }
 
-            // We could try to split in same record, then multiple
-            // splits will be data.len(), dedup to remove those
-            splits.dedup();
+        splits.dedup();
 
-            // Could end up with less threads than splits
-            let actual_threads = splits.len() - 1;
+        // Could end up with less threads than splits
+        let actual_threads = splits.len().saturating_sub(1);
+        assert!(actual_threads > 0);
 
-            let mut handles = Vec::with_capacity(actual_threads);
-            for i in 0..actual_threads {
-                let mut worker_processor = processor.clone();
-                let start = splits[i];
-                let end = splits[i + 1];
+        let ranges: Vec<(usize, usize)> = splits.windows(2).map(|w| (w[0], w[1])).collect();
 
-                let handle = scope.spawn(move || {
-                    let mut parser = FastxParser::<CONFIG>::from_slice(&data[start..end]);
-                    let mut count = 0;
+        let mut processors = Vec::with_capacity(actual_threads);
+        for _ in 0..actual_threads {
+            processors.push(processor.clone());
+        }
+
+        let pool = ThreadPoolBuilder::new()
+            .num_threads(actual_threads)
+            .build()
+            .map_err(|_| ProcessError::Process("failed to build rayon pool".into()))?;
+
+        pool.install(|| {
+            // parallel iterate ranges zipped with processors
+            ranges
+                .into_par_iter()
+                .zip(processors.into_par_iter())
+                .map(|((start, end), mut worker_processor)| {
+                    let slice = &data[start..end];
+                    let mut parser = FastxParser::<CONFIG>::from_slice(slice);
+                    let mut count = 0usize;
 
                     while let Some(_event) = parser.next() {
                         worker_processor.process_record(&parser)?;
@@ -117,18 +132,10 @@ impl<'b, const CONFIG: Config> paraseq::parallel::ParallelReader
                         worker_processor.on_batch_complete()?;
                     }
                     worker_processor.on_thread_complete()?;
-                    Ok(())
-                });
-                handles.push(handle);
-            }
-            for handle in handles {
-                match handle.join() {
-                    Ok(Ok(())) => (),
-                    Ok(Err(e)) => return Err(e),
-                    Err(_) => return Err(ProcessError::JoinError),
-                }
-            }
-            Ok(())
+                    Ok::<(), paraseq::ProcessError>(())
+                })
+                .collect::<Result<Vec<_>, _>>()
+                .map(|_v| ())
         })
     }
 
